6e858d90fcf89140aa68cfd7f066f72fdaf838b6
Rudra <rudra.phymos@gmail.com>
2026-06-07 17:57:05 +0530

    fixed

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,26 @@
 import { MongoClient, Db } from 'mongodb'
 
-const uri = process.env.MONGODB_URI!
+let clientPromise: Promise<MongoClient> | null = null
 
-let clientPromise: Promise<MongoClient>
+function getClientPromise(): Promise<MongoClient> {
+  if (clientPromise) return clientPromise
 
-if (process.env.NODE_ENV === 'development') {
-  if (!(global as any)._mongoClientPromise) {
-    ;(global as any)._mongoClientPromise = new MongoClient(uri).connect()
+  const uri = process.env.MONGODB_URI
+  if (!uri) throw new Error('MONGODB_URI environment variable is not set')
+
+  if (process.env.NODE_ENV === 'development') {
+    if (!(global as any)._mongoClientPromise) {
+      ;(global as any)._mongoClientPromise = new MongoClient(uri).connect()
+    }
+    clientPromise = (global as any)._mongoClientPromise
+  } else {
+    clientPromise = new MongoClient(uri).connect()
   }
-  clientPromise = (global as any)._mongoClientPromise
-} else {
-  clientPromise = new MongoClient(uri).connect()
+
+  return clientPromise!
 }
 
 export async function getDb(): Promise<Db> {
-  const client = await clientPromise
+  const client = await getClientPromise()
   return client.db()
 }
